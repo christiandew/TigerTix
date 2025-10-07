d68bdc0889bc4f3ceff97fe36d88dd81fc4d1e29
christiandew <cddew@clemson.edu>
2025-10-07 15:00:15 -0400

    Starter Code and Strcture

---
+++ backend/routes/routes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const { listEvents } = require('../controllers/controller');
+router.get('/events', listEvents);
+module.exports = router;
+
